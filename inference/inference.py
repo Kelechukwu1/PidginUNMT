ac24c647215a3bd59188c152f690fdb0c099bc21
Kelechukwu <kelechiogueji@gmail.com>
2019-08-27 21:16:23 +0100

    began inference script

---
+++ inference/inference.py
@@ -0,0 +1,119 @@
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+#
+
+import os
+import subprocess
+from collections import OrderedDict
+from logging import getLogger
+import numpy as np
+import torch
+from torch import nn
+logger = getLogger()
+from src.evaluator import EvaluatorMT
+
+
+TOOLS_PATH = '../tools'
+BLEU_SCRIPT_PATH = os.path.join(TOOLS_PATH, 'mosesdecoder/scripts/generic/multi-bleu.perl')
+assert os.path.isfile(BLEU_SCRIPT_PATH), "Moses not found. Please be sure you downloaded Moses in %s" % TOOLS_PATH
+
+class Inference(EvaluatorMT):
+    
+    def __init__(self, trainer, data, params, parallel:bool):
+        """
+        Initialize evaluator.
+        """
+        self.encoder = trainer.encoder
+        self.decoder = trainer.decoder
+        self.discriminator = trainer.discriminator
+        self.data = data
+        self.dico = data['dico']
+        self.params = params
+        self.parallel = parallel
+
+        # create reference files for BLEU evaluation
+        self.create_reference_files()
+    
+    def create_reference_files(self):
+        """
+        Create reference files for BLEU evaluation.
+        """
+        params = self.params
+        params.ref_paths = {}
+
+        for (lang1, lang2), _ in self.data['para'].items():
+
+            assert lang1 < lang2
+            lang1_id = params.lang2id[lang1]
+            lang2_id = params.lang2id[lang2]
+
+            for data_type in ['valid', 'test']:
+
+                lang1_path = os.path.join(params.dump_path, 'ref.{0}-{1}.{2}.txt'.format(lang2, lang1, data_type))
+                lang2_path = os.path.join(params.dump_path, 'ref.{0}-{1}.{2}.txt'.format(lang1, lang2, data_type))
+
+                lang1_txt = []
+                lang2_txt = []
+
+                # convert to text
+                for (sent1, len1), (sent2, len2) in self.get_iterator(data_type, lang1, lang2):
+                    lang1_txt.extend(convert_to_text(sent1, len1, self.dico[lang1], lang1_id, params))
+                    lang2_txt.extend(convert_to_text(sent2, len2, self.dico[lang2], lang2_id, params))
+
+                # replace <unk> by <<unk>> as these tokens cannot be counted in BLEU
+                lang1_txt = [x.replace('<unk>', '<<unk>>') for x in lang1_txt]
+                lang2_txt = [x.replace('<unk>', '<<unk>>') for x in lang2_txt]
+
+                # export hypothesis
+                with open(lang1_path, 'w', encoding='utf-8') as f:
+                    f.write('\n'.join(lang1_txt) + '\n')
+                with open(lang2_path, 'w', encoding='utf-8') as f:
+                    f.write('\n'.join(lang2_txt) + '\n')
+
+                # restore original segmentation
+                restore_segmentation(lang1_path)
+                restore_segmentation(lang2_path)
+
+                # store data paths
+                params.ref_paths[(lang2, lang1, data_type)] = lang1_path
+                params.ref_paths[(lang1, lang2, data_type)] = lang2_path
+
+    
+def eval_moses_bleu(ref, hyp):
+    """
+    Given a file of hypothesis and reference files,
+    evaluate the BLEU score using Moses scripts.
+    """
+    assert os.path.isfile(ref) and os.path.isfile(hyp)
+    command = BLEU_SCRIPT_PATH + ' %s < %s'
+    p = subprocess.Popen(command % (ref, hyp), stdout=subprocess.PIPE, shell=True)
+    result = p.communicate()[0].decode("utf-8")
+    if result.startswith('BLEU'):
+        return float(result[7:result.index(',')])
+    else:
+        logger.warning('Impossible to parse BLEU score! "%s"' % result)
+        return -1
+
+
+def convert_to_text(batch, lengths, dico, lang_id, params):
+    """
+    Convert a batch of sentences to a list of text sentences.
+    """
+    batch = batch.cpu().numpy()
+    lengths = lengths.cpu().numpy()
+    bos_index = params.bos_index[lang_id]
+
+    slen, bs = batch.shape
+    assert lengths.max() == slen and lengths.shape[0] == bs
+    assert (batch[0] == bos_index).sum() == bs
+    assert (batch == params.eos_index).sum() == bs
+    sentences = []
+
+    for j in range(bs):
+        words = []
+        for k in range(1, lengths[j]):
+            if batch[k, j] == params.eos_index:
+                break
+            words.append(dico[batch[k, j]])
+        sentences.append(" ".join(words))
+    return sentences        
